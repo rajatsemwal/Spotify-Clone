64ce8fd33c1d3ce6a2247b6cf85416c14d4efbcb
Rajat Semwal <66676396+LordCaesar03@users.noreply.github.com>
2021-05-20 14:19:59 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const SpotifyWebApi = require('spotify-web-api-node')
+const lyricsFinder = require('lyrics-finder')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/refresh', (req, res) => {
+
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
+            })
+            spotifyApi.setAccessToken(data.body['access_token'])
+        }).catch(() => {
+            res.sendStatus(400)
+        })
+})
+
+app.post('/login', (req, res) => {
+
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(err => {
+        res.sendStatus(400)
+    })
+})
+
+app.get('/lyrics', async (req, res) => {
+    const lyrics = (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics found!"
+    res.json({lyrics})
+})
+
+app.listen(3001)
